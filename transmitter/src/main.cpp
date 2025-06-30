5a3c2cc9d7e03aff473e82bd690d1ad95ef52fe3
Kemal <kemal.usanmaz@tai.com.tr>
2025-06-30 16:44:19 +0300

    gitignore file is updated

---
+++ transmitter/src/main.cpp
@@ -0,0 +1,38 @@
+#include "../../configuration/include/configuration.hpp"
+#include "../include/transmitter.hpp"
+#include <iostream>
+#include <string>
+#include <termios.h>
+
+int main() {
+  RSConfiguration config;
+  config.initialize(config.getJsonData()["transmitterDeviceName"]);
+  config.setRsConfig(config.getJsonData()["baudrate"]);
+  RSTransmit transmitter(config.getFd());
+  transmitter.sendData("\b\b");
+  int confOption;
+  while (true) {
+    std::cout << "---------RS Transmitter---------" << std::endl;
+    std::cout << "0- Send Data\n"
+                 "1- Exit\n"
+              << std::endl;
+    std::cout << "Enter a valid number: ";
+    std::cin >> confOption;
+    std::cout << std::endl;
+    if (confOption == 0) {
+      while (true) {
+        std::string userData;
+        std::cout << "Data: ";
+        std::cin >> userData;
+        transmitter.sendData(userData);
+        continue;
+      }
+      continue;
+    } else if (confOption == 1) {
+      break;
+    } else {
+      std::cout << "Invalid number. Please Choose Proper Number" << std::endl;
+      continue;
+    }
+  }
+}
---
+++ transmitter/src/transmitter.cpp
@@ -0,0 +1,19 @@
+#include "../include/transmitter.hpp"
+#include <iostream>
+#include <unistd.h> // write
+
+RSTransmit::RSTransmit(int fd) : m_fd(fd) {}
+
+void RSTransmit::sendData(std::string data) {
+  data += "\n"; // Append a newline character to the data.
+  // The write() system call attempts to write data to a file descriptor.
+  auto bytesWritten = write(m_fd, data.c_str(), data.length());
+  if (bytesWritten < 0) { // Check if the write operation resulted in an error.
+    std::cerr << "Write error!\n";
+  } else {
+    // Print the transmitted data and its length to the console for
+    // confirmation.
+    std::cout << "Transmit: " << data << std::endl;
+    std::cout << "Transmit Data Length: " << data.size() << std::endl;
+  }
+}
